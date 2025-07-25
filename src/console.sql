fd87311cba3c28357032441b4f8f6e0f0c50197d
xenophorus <xenophorus@gmail.com>
2025-07-26 04:33:57 +1000

    1

--- src/console.sql
+++ src/console.sql
@@ -1,9 +1,11 @@
+drop view if exists products_to_distribute;
 drop table if exists products;
-drop table if exists branch_product;
-drop table if exists rc_product;
 drop table if exists log_days;
 drop table if exists stores;
 drop table if exists needs;
+drop table if exists branch_product;
+drop table if exists rc_product;
+drop table if exists current_branch_data;
 
 create table branch_product
 (
@@ -49,6 +51,14 @@ create table needs
     needs      int
 );
 
+create table current_branch_data
+(
+    branch_id     uuid not null,
+    prod_sum      int,
+    branch_volume int
+)
+;
+
 -- grant pg_read_server_files to alex;
 
 copy products (product_id, category_id)
@@ -77,9 +87,11 @@ insert into log_days(select b.branch_id,
                      group by b.branch_id, p.category_id)
 ;
 
-insert into stores (select distinct branch_id,
-                                    ceil(random() * 3) as priority
-                    from log_days)
+insert into stores (with cte as (select distinct branch_id
+                                 from branch_product)
+                    select branch_id,
+                           ceil(random() * 3) as priority
+                    from cte)
 ;
 
 
@@ -89,60 +101,121 @@ insert into stores (select distinct branch_id,
 --        7 as logdays
 -- from branch_product b join products p on b.product = p.product_id
 -- ;
-
+-- ПЕРЕПИСАТЬ! Пляшем от товара на складе, оптимизации оттуда же
+-- обязательно фильтрацию на rc и p
 insert into needs(branch_id, product_id, needs)
-    (with recursive
-         categories as (select distinct ld.category_id, ld.log_days
-                        from log_days as ld),
-         br_reduced as
-             (select br.product_id as product_id,
-                     br.branch_id  as branch_id,
-                     br.reserve    as branch_reserve,
-                     br.remain     as branch_remain,
-                     c.log_days    as log_days
-              from branch_product as br
-                       join rc_product as rc on br.product_id = rc.product_id
-                       join products as p on br.product_id = p.product_id
-                       join categories as c on p.category_id = c.category_id
-              where br.remain > 0
-                 or br.reserve > 0
-                 or br.transit > 0
-                 or rc.remain > 0
-                 or rc.reserve > 0
-                 or rc.transit > 0)
+    (with br_reduced as
+              (select br.product_id as product_id,
+                      br.branch_id  as branch_id,
+                      br.reserve    as branch_reserve,
+                      br.remain     as branch_remain,
+                      p.category_id as category_id
+               from branch_product as br
+                        join rc_product as rc on br.product_id = rc.product_id
+                        join products as p on br.product_id = p.product_id
+               where rc.remain > 0) -- какая разница, сколько надо товара, которого нет
      select brd.branch_id,
             brd.product_id,
             case -- min(1, x) * Logdays
-                when 150 < brd.branch_remain + brd.branch_reserve
-                    then ceil((150 * brd.log_days) * random())
-                else ceil(((brd.branch_remain + brd.branch_reserve) * brd.log_days) * random())
+                when brd.branch_remain > 150
+                    then ceil((150 * l.log_days) * random())
+                else ceil(((brd.branch_remain) * l.log_days) * random())
                 end as need
      from br_reduced as brd
-     );
+              join log_days as l on brd.category_id = l.category_id
+         and l.branch_id = brd.branch_id);
 
-with recursive
-    categories as (select distinct ld.category_id, ld.log_days
-                   from log_days as ld),
-    br_reduced as
-        (select br.product_id as product_id,
-                br.branch_id  as branch_id,
-                p.category_id as category_id,
-                br.transit    as branch_transit,
-                br.reserve    as branch_reserve,
-                br.remain     as branch_remain,
-                rc.transit    as rc_transit,
-                rc.remain     as rc_remain,
-                rc.reserve    as rc_reserve,
-                c.log_days    as log_days
-         from branch_product as br
-                  join rc_product as rc on br.product_id = rc.product_id
-                  join products as p on br.product_id = p.product_id
-                  join categories as c on p.category_id = c.category_id
-         where br.remain > 0
-            or br.reserve > 0
-            or br.transit > 0
-            or rc.remain > 0
-            or rc.reserve > 0
-            or rc.transit > 0)
+/*
+Есть минимум два транзита по 3000, со склада и из филиала, придется их учесть для каждого филиала.
+Также придется допустить, что на каждый филиал может быть только один транзит со склада и только один
+транзит с филиала.
+*/
+
+insert into current_branch_data
+    (with prod_volume as
+              (select sum(remain) as prod_sum,
+                      branch_id
+               from branch_product
+               group by branch_id
+               order by prod_sum),
+     max_br_transit as (select max(transit) as m from branch_product),
+     max_rc_transit as (select max(transit) as m from rc_product)
+     select branch_id,
+            prod_sum +
+            (select m from max_br_transit) +
+            (select m from max_rc_transit),
+            case
+                when prod_sum +
+                     (select m from max_br_transit) +
+                     (select m from max_rc_transit) < 15000 then 15000
+                when prod_sum +
+                     (select m from max_br_transit) +
+                     (select m from max_rc_transit) < 25000 then 25000
+                else 42000
+                end as branch_volume
+     from prod_volume)
+;
+
+with cte as (
+select cd.branch_id as branch_id,
+       prod_sum,
+       branch_volume,
+       priority
+from current_branch_data as cd
+         join stores as s on cd.branch_id = s.branch_id)
+select *,
+       round(avg(prod_sum) over (partition by branch_volume), 1) as avg_prods
+from cte
+;
+
+
 select count(*)
-from br_reduced as brd;
+from current_branch_data;
+
+select count(distinct branch_id)
+from branch_product;
+
+select count(*)
+from needs;
+
+select count(*)
+from stores;
+
+select b.transit as from_branch,
+       r.transit as from_rc,
+       b.product_id,
+       b.branch_id
+from branch_product as b join rc_product as r on b.product_id = r.product_id
+where b.transit > 0 or r.transit > 0
+order by from_rc desc
+;
+
+drop view if exists products_to_distribute;
+/*
+Хотят больше, чем могут вместить - коэффициент?  min_shipment
+
+
+*/
+create materialized view products_to_distribute as (select b.product_id as product_id,
+       b.branch_id as branch_id,
+       b.remain as br_rem,
+       r.remain as stock_rem,
+       n.needs as needs,
+       s.priority as priority,
+       cbd.branch_volume,
+       cbd.branch_volume - cbd.prod_sum as room_in_br,
+       sum(needs) over (partition by b.branch_id) as br_total_need
+from branch_product as b join rc_product as r on b.product_id = r.product_id
+join stores s on b.branch_id = s.branch_id
+join current_branch_data cbd on b.branch_id = cbd.branch_id
+join needs as n on b.branch_id = n.branch_id and b.product_id = n.product_id
+where r.remain > 0)
+;
+
+select *
+from products_to_distribute
+where product_id = '6c0f29b3-b653-11e8-9547-00155d03330d'
+order by priority, branch_volume;
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,19 +1,55 @@
-import psycopg2
-
+from django.db.models.expressions import result
 from sqlalchemy import create_engine, text
 
-engine = create_engine(f'postgresql://alex:21779835@localhost:5435/dnstesttask')
+USERNAME = "alex"
+PASSWORD = "21779835"
+HOST = "localhost"
+PORT = "5435"
+DATABASE = "dnstesttask"
 
-with engine.connect() as connection:
-    result = connection.execute(text("select * from stores"))
-    for row in result:
-        print(row)
-    print(1)
+class ProductDistribution:
+    def __init__(self):
+        self.engine = create_engine(f'postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}')
+        self.branches_num = self.get_branches()
+        self.products_num = self.get_products()
+        self.run_distribution()
+
+    def get_db_data(self, txt):
+        with self.engine.connect() as connection:
+            return connection.execute(text(txt)).fetchall()
+
+
+    def get_branches(self):
+        result = self.get_db_data("select distinct branch_id from branch_product")
+        return result[0][0]
+
+    def get_products(self):
+        result = self.get_db_data("select distinct product_id from rc_product where remain > 0")
+        return result
+
+    def run_distribution(self):
+        for product in self.products_num:
+            product_uuid = str(product[0])
+            txt = f'''
+            select * from products_to_distribute
+            where product_id = '{product_uuid}'
+            order by priority, branch_volume;
+            '''
+            result = self.get_db_data(txt)
+
+            print(product)
+
+# with engine.connect() as connection:
+#     result = connection.execute(text("select * from stores"))
+#     for row in result:
+#         print(row)
+#     print(1)
 
 
 
 def main():
-    ...
+    pd = ProductDistribution()
+
 
 
 if __name__ == '__main__':
